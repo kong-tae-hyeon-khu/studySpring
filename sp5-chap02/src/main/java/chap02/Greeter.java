6db6b7cd872a02aa96922914f8b187b8a6026d8e
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-03-26 15:46:28 +0900

    1st Package

--- sp5-chap02/src/main/java/chap02/Greeter.java
+++ sp5-chap02/src/main/java/chap02/Greeter.java
@@ -5,7 +5,7 @@ public class Greeter {
     private String format;
 
     public String greet(String guest) {
-        return String.format(format, guest);
+        return String.format("%s %s", format, guest);
     }
 
     public void setFormat(String format) {
