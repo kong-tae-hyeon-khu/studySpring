ba7f618d4f5fdb992c84c0a6d5f3a448c3fcaf2f
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-03-27 15:30:44 +0900

    Simple Member Handling Project

--- sp5-chap03/src/main/java/spring/MemberRegisterService.java
+++ sp5-chap03/src/main/java/spring/MemberRegisterService.java
@@ -1,1 +1,21 @@
 package spring;
+import java.time.LocalDateTime;
+public class MemberRegisterService {
+    private MemberDao memberDao;
+
+    public MemberRegisterService(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    public Long regist(RegisterRequest req) {
+        Member member = memberDao.selectByEmail(req.getEmail());
+        if (member != null) { // 이미 존재한다면,
+            throw new DuplicateMemberException("Email 중복");
+        }
+        Member newMember = new Member(
+                req.getEmail(), req.getPassword(), req.getName(), LocalDateTime.now()
+        );
+        memberDao.insert(newMember);
+        return newMember.getId();
+    }
+}
